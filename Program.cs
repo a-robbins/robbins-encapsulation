0dbdcc1e669f4f2e25edabe559b8999ca7bf8f75
a-robbins <45301147+a-robbins@users.noreply.github.com>
2021-02-13 19:16:12 -0800

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace robbinsEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student a = new Student("A");
+
+            a.Start = "2/5/2010";
+        }
+    }
+}
